b6a41f4b95586ee3ec3a297b51abbb58f4f71bd2
Chandan Chaudhary <chandanchaudhary@outlook.com>
2021-10-06 23:27:36 +0530

    solution(1.3 - URLify): write a function which replaces all spaces inside a string with `%20`

---
+++ src/Chapter 01 - Arrays and Strings/1.3 URLify/index.test.ts
@@ -0,0 +1,44 @@
+import {
+    URLify
+} from '.';
+
+/**
+ * Compare equality of two strings
+ *
+ * @param str1 One input string
+ * @param str2 Another input string
+ * @returns `true` if both strings are equal, otherwise `false`
+ */
+function areStringsEqual(str1: string[], str2: string[]): boolean {
+    if (str1.length !== str2.length) return false;
+
+    for (let i = 0; i < str1.length; i++) {
+        const char = str1[i];
+        const expectedChar = str2[i];
+        if (char !== expectedChar) return false;
+    }
+
+    return true;
+}
+
+describe('URLify', () => {
+    it('doesn\'t change anything when there is no space in the string', () => {
+        const str = ["M", "r", "J", "o", "h", "n", "S", "m", "i", "t", "h"];
+        const size = 11;
+
+        URLify(str, size);
+        const expectedResult = ["M", "r", "J", "o", "h", "n", "S", "m", "i", "t", "h"];
+
+        expect(areStringsEqual(str, expectedResult)).toBe(true);
+    });
+
+    it('replaces all spaces with `%20` when string has spaces', () => {
+        const str = ["M", "r", " ", "J", "o", "h", "n", " ", "S", "m", "i", "t", "h", "", "", "", ""];
+        const size = 13;
+
+        URLify(str, size);
+        const expectedResult = ["M", "r", "%", "2", "0", "J", "o", "h", "n", "%", "2", "0", "S", "m", "i", "t", "h"];
+
+        expect(areStringsEqual(str, expectedResult)).toBe(true);
+    });
+});
---
+++ src/Chapter 01 - Arrays and Strings/1.3 URLify/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Replaces all spaces with `%20` characters inside a string
+ *
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ *
+ * @param str - The input string with extra length to accomodate the space covered by `%20`
+ * @param size - true size of the string (actual string without extra length at the end)
+ * @returns nothing, In place operation
+ */
+export function URLify(str: string[], size: number) {
+    if (size === 0) return str;
+    let j = str.length-1;
+
+    for (let i = size-1; i >= 0; i--) {
+        const char = str[i];
+        if (char === " ") {
+            str[j--] = "0";
+            str[j--] = "2";
+            str[j--] = "%";
+        } else {
+            str[j] = char;
+            j--;
+        }
+    }
+
+    return str;
+}
