527c458a45178fa60b9a615c22684687ae5092cd
Chandan Chaudhary <chandanchaudhary@outlook.com>
2021-10-07 23:04:39 +0530

    solution(1.4 - Palindrone Permutation): write a function to check if a string has palindrome permutation

---
+++ src/Chapter 01 - Arrays and Strings/1.4 Palindrome Permutation/index.ts
@@ -0,0 +1,33 @@
+/**
+ * Determines if the string has palindrome permutation
+ *
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ *
+ * @param str The input string
+ * @returns `true` if the string has a permutation which is a palidrome, otherwise `false`
+ */
+export function isPalindroneOfPermutation(str: string): boolean {
+    if (str.length <= 1) return true;
+
+    let countOdd = 0;
+    const charFreq = Array.from({ length: 26 }, () => 0);
+    const charCodeA = "a".charCodeAt(0);
+
+
+    for (let ch of str) {
+        let charCode = ch.toLowerCase().charCodeAt(0) - charCodeA;
+
+        if (charCode >= 0 && charCode <= 25) {
+            charFreq[charCode] += 1;
+
+            if (charFreq[charCode] % 2 === 1) {
+                countOdd++;
+            } else {
+                countOdd--;
+            }
+        }
+    }
+
+    return countOdd <= 1;
+}
